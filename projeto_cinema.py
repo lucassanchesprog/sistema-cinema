f4ae49e499dee21f6b990acdfe63e587fce63952
Lucas Sanches <lucassanches5376@gmail.com>
2026-03-03 20:56:33 -0300

    Add files via upload

---
+++ projeto_cinema.py
@@ -0,0 +1,22 @@
+print("Bem-vindo ao Cinema Python!")
+print("Hoje temos em cartaz os seguintes filmes:")
+
+print("1 - Vingadores")
+print("2 - Frozen")
+print("3 - Carros")
+
+escolha = input("Qual será sua escolha? ")
+
+print("Agora preciso dos seus dados para gerar o ingresso.")
+
+nome = input("Nome completo: ")
+idade = input("Sua idade: ")
+
+print("Seu ingresso está pronto!")
+print("------ INGRESSO ------")
+print("Sala: 7")
+print("Filme escolhido:", escolha)
+print("Nome:", nome)
+print("Idade:", idade)
+print("----------------------")
+print("O Cinema Python agradece. Aproveite o filme!")
